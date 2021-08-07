7ac6a370f62b90ad2f7af7512adfe1922f21adf0
xianggaofeng <xianggaofeng@excean.com>
2021-08-07 15:37:04 +0800

    sync modify

---
+++ compat/src/main/java/com/zero/support/simple/CellFragment.java
@@ -0,0 +1,88 @@
+package com.zero.support.simple;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.zero.support.compat.R;
+import com.zero.support.compat.app.SupportFragment;
+import com.zero.support.compat.databinding.ActivityCellBinding;
+import com.zero.support.compat.recycler.Cell;
+import com.zero.support.compat.recycler.CellAdapter;
+import com.zero.support.compat.vo.Resource;
+import com.zero.support.simple.widget.ResourceLayout;
+
+import java.util.List;
+
+public class CellFragment extends SupportFragment {
+    private CellAdapter adapter;
+    private RecyclerView recyclerView;
+    private CellViewModel<?> viewModel;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        ActivityCellBinding binding = DataBindingUtil.inflate(inflater, R.layout.activity_cell, container, false);
+        viewModel = onCreateViewModel();
+        adapter = new CellAdapter(viewModel);
+        onCreateContentView(LayoutInflater.from(requireActivity()), (ResourceLayout) binding.getRoot());
+
+
+        viewModel.resource().asLive().observe(getViewLifecycleOwner(), new Observer<Resource<List<Cell>>>() {
+            @Override
+            public void onChanged(Resource<List<Cell>> listResource) {
+                binding.setResource(listResource);
+                if (listResource.isSuccess()) {
+                    adapter.submitList(listResource.data);
+                }
+            }
+        });
+        return binding.getRoot();
+    }
+
+    public CellViewModel<?> getViewModel() {
+        return viewModel;
+    }
+
+    protected void onCreateContentView(LayoutInflater from, FrameLayout content) {
+        RecyclerView recyclerView = new RecyclerView(requireActivity());
+        content.addView(recyclerView, new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        setUpRecyclerView(recyclerView);
+    }
+
+    protected void setUpRecyclerView(RecyclerView recyclerView) {
+        this.recyclerView = recyclerView;
+        onBindRecyclerView(recyclerView);
+        onBindCellAdapter(adapter);
+        recyclerView.setAdapter(adapter);
+    }
+
+    protected void onBindRecyclerView(RecyclerView recyclerView) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(requireActivity()));
+    }
+
+    protected void onBindCellAdapter(CellAdapter adapter) {
+    }
+
+    public RecyclerView getRecyclerView() {
+        return recyclerView;
+    }
+
+    public CellAdapter getAdapter() {
+        return adapter;
+    }
+
+    protected CellViewModel<?> onCreateViewModel() {
+        return attachSupportViewModel(CellViewModel.class);
+    }
+}
